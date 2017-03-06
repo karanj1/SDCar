e107e5d678a5c6b18a18aaa6e0246e9525460578
Ubuntu <karanj@DSVM.tq21k3mcjisudayzuc1iutt5lg.bx.internal.cloudapp.net>
2017-03-06 13:39:09 +0000

    p3 test.py

---
+++ CarND-Behavioral-Cloning-P3/test.py
@@ -0,0 +1,32 @@
+import csv
+import cv2
+import numpy as np
+
+lines = []
+with open('data/driving_log_test.csv') as csvfile:
+	reader = csv.reader(csvfile)
+	for line in reader:
+		lines.append(line)
+
+images = []
+measurements = []
+for line in lines:
+	source_path = line[0]	#column 0 in csv
+	filename = source_path.split('/')[-1]	#take filename from whole path
+	current_path = 'data/IMG_test/' + filename
+	image = cv2.imread(current_path)
+	images.append(image)
+	measurement = float(line[3])	#Coulumn 3 : Steering angle
+	measurements.append(measurement)
+	X_train = np.array(images)
+	y_train = np.array(measurements)
+
+
+model = Sequential()
+model.add(Flatten(input_shape=(160,320,3)))
+model.add(Dense(1))
+
+model.compile(loss='mse', optimizer='adam')
+model.fit(X_train, Y_train,validation_split=0.2, shuffle=True)
+
+model.save('model_test.h5')
