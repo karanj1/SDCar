7721f4ae9040cdf57af651eadf337c68591b6c67
karanj1 <karanj54321@gmail.com>
2017-03-09 02:13:26 -0800

    p3 test.py 00D

--- CarND-Behavioral-Cloning-P3/test.py
+++ CarND-Behavioral-Cloning-P3/test.py
@@ -2,7 +2,9 @@ import csv
 import cv2
 import numpy as np
 from keras.models import Sequential
-from keras.layers.core import Dense, Flatten, Activation
+from keras.layers.core import Dense, Flatten, Activation, Dropout
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
 
 lines = []
 with open('data/driving_log_test.csv') as csvfile:
@@ -17,16 +19,28 @@ for line in lines:
 	filename = source_path.split('\\')[-1]	#take filename from whole path
 	current_path = 'data/IMG_test/' + filename
 	image = cv2.imread(current_path)
+	image_flipped = np.fliplr(image)
 	images.append(image)
+	images.append(image_flipped)
 	measurement = float(line[3])	#Coulumn 3 : Steering angle
+	measurement_flipped = -measurement  #Steering angle for flipped image
 	measurements.append(measurement)
+	measurements.append(measurement_flipped)
 
 X_train = np.array(images)
 y_train = np.array(measurements)
 print(X_train.shape)
 
 model = Sequential()
-model.add(Flatten(input_shape=(160,320,3)))
+model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape=(160,320,3)))
+model.add(Convolution2D(6,5,5, activation="relu"))
+model.add(MaxPooling2D())		#or can be MaxPooling2D((2, 2))
+model.add(Convolution2D(6,5,5, activation="relu"))
+model.add(MaxPooling2D())
+model.add(Flatten())
+model.add(Dense(128))
+model.add(Dropout(0.5))
+model.add(Dense(75))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
